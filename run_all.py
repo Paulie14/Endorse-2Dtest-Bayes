795c15cf20ce854474ad6972b317e49751173d17
Paulie14 <pavelexner@gmail.com>
2022-01-21 11:16:25 +0100

    Improve assembly of metacentrum scripts.
    - additional se paths in run_all relative to main rep dir

--- run_all.py
+++ run_all.py
@@ -5,6 +5,8 @@ import ruamel.yaml as yaml
 
 import aux_functions
 
+# this script is supposed to be dependent only on python packages present on any machine
+# all other python scripts are later run inside docker container
 
 def setup():
     # create and cd workdir
@@ -41,9 +43,8 @@ if __name__ == "__main__":
     # setup paths and directories
     config_dict = setup()
 
-    # RUN THE MCMC SIMULATION
     # default parameters
-    N = 2  # number of sampling processes
+    N = 2  # default number of sampling processes
     oversubscribe = False  # if there are not enough slots available
     visualize = False  # True = only visualization
     problem_path = None
@@ -59,6 +60,8 @@ if __name__ == "__main__":
         visualize = sys.argv[3] == "visualize"
 
     # run sampling
+    # paths are relative to repository dir
+    # paths passed to surrDAMH are absolute
     command = None
     if visualize:
         # os.error("Visualization not implemented.")
@@ -72,10 +75,12 @@ if __name__ == "__main__":
             opt = " --oversubscribe "
         else:
             opt = " "
-        sampler = "python3 -m mpi4py surrDAMH/process_SAMPLER.py "
-        solver = "python3 -m mpi4py surrDAMH/process_SOLVER.py " + problem_path + " "
-        collector = "python3 -m mpi4py surrDAMH/process_COLLECTOR.py "
+        sampler = "python3 -m mpi4py surrDAMH/surrDAMH/process_SAMPLER.py "
+        solver = "python3 -m mpi4py surrDAMH/surrDAMH/process_SOLVER.py " + problem_path + " "
+        collector = "python3 -m mpi4py surrDAMH/surrDAMH/process_COLLECTOR.py "
 
+        # prepare running command for local run
+        # or prepare PBS script for running on Metacentrum
         if not config_dict["run_on_metacentrum"]:
             # possibly change to particular mpirun for testing
             # mpirun = "/usr/local/mpich_3.4.2/bin/mpirun"
@@ -84,65 +89,70 @@ if __name__ == "__main__":
                       + ":" + " -n 1" + opt + solver + ":" + " -n 1" + opt + collector
         else:
             met = config_dict["metacentrum"]
-            ncpus = met["ncpus"]
+            common_lines = [
+                '# run from the repository directory',
+                'cd "' + config_dict["script_dir"] + '"',
+                '\n# command for running correct docker image',
+                'image=$(./endorse_fterm image)',
+                'sing_command="singularity exec -B ' + met["workspace_rel"] + '/:/' + met["workspace_rel"]
+                        + ' docker://$image"',
+                '\n# auxiliary command for opening Python environment inside docker image',
+                'bash_py="bash -c \'source ./venv/bin/activate &&"',
+            ]
+
+            # prepare preprocess script
+            lines = [
+                '#!/bin/bash\n',
+                *common_lines, '\n',
+                'command="$sing_command $bash_py ' + 'python3 preprocess.py ' + problem_path + '\'"',
+                '\n', 'echo $command', 'eval $command'
+            ]
+            with open("shell_preprocess.sh", 'w') as f:
+                f.write('\n'.join(lines))
+
+            # prepare PBS script
             lines = [
                 '#!/bin/bash',
                 '#PBS -S /bin/bash',
-                '#PBS -l select=1:ncpus=' + str(ncpus+2) + ':mem=12gb',
+                '#PBS -l select=1:ncpus=' + str(N+2) + ':mem=12gb',
                 '#PBS -l walltime=' + met["walltime"],
                 '#PBS -q ' + met["queue"],
                 '#PBS -N ' + met["name"],
                 '#PBS -j oe',
                 '\n',
-                'cd "' + config_dict["script_dir"] + '"',
-                'image=$(./endorse_fterm image)',
-                'sing_command="singularity exec -B ' + met["workspace_rel"] + '/:/' + met["workspace_rel"]
-                        + ' docker://$image"',
-                '\n',
-                'bash_py="bash -c \'source ./venv/bin/activate &&"',
+                *common_lines,
+                '\n# define surrDaMH processes',
                 'sampler="$bash_py ' + sampler + '\'"',
                 'solver="$bash_py ' + solver + '\'"',
                 'collector="$bash_py ' + collector + '\'"',
                 '\n',
+                '# load correct MPI lib',
                 'module load mpich-3.0.2-gcc',
                 'which mpirun',
                 'mpirun --version',
+                '\n# finally gather the full command',
                 'command="mpirun '
-                        + '-n ' + str(ncpus) + ' $sing_command $sampler : '
+                        + '-n ' + str(N) + ' $sing_command $sampler : '
                         + '-n 1 $sing_command $solver : '
                         + '-n 1 $sing_command $collector"',
-                'echo $command',
-                'eval $command'
+                '\n', 'echo $command', 'eval $command'
             ]
             with open("shell_process.sh", 'w') as f:
                 f.write('\n'.join(lines))
 
-            lines = [
-                '#!/bin/bash',
-                'cd "' + config_dict["script_dir"] + '"',
-                'image=$(./endorse_fterm image)',
-                'sing_command="singularity exec -B ' + met["workspace_rel"] + '/:/' + met["workspace_rel"]
-                + ' docker://$image"',
-                '\n',
-                'bash_py="bash -c \'source ./venv/bin/activate &&"',
-                'sampler="$sing_command $bash_py ' + ' python3 preprocess.py ' + problem_path + '\'"',
-                '\n',
-                'echo $command',
-                'eval $command'
-            ]
-            with open("shell_preprocess.sh", 'w') as f:
-                f.write('\n'.join(lines))
-
+    # final command call
     if config_dict["run_on_metacentrum"]:
-        # TODO: run preprocess through singularity
+        # these are called from actual simulation directory
+        # preprocess
         os.system(os.path.join(config_dict["work_dir"], "shell_preprocess.sh"))
+        # PBS script
         os.system("qsub " + os.path.join(config_dict["work_dir"], "shell_process.sh"))
     else:
+        # preprocess
         from preprocess import preprocess
         preprocess(config_dict, problem_path)
 
-        rep_dir = config_dict["script_dir"]
-        os.chdir(os.path.join(rep_dir, "surrDAMH"))
+        # local command call
+        os.chdir(config_dict["script_dir"])
         print(command)
-        # exit(0)
         os.system(command)
