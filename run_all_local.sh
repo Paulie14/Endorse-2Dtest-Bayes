eb591d271b3ec59af5d457d64aef15c26762a7df
Pavel Exner <pavelexner@gmail.com>
2022-02-18 12:28:42 +0100

    Enable running locally both in docker and singularity.

--- run_all_local.sh
+++ run_all_local.sh
@@ -20,6 +20,13 @@ elif [ "$4" == "run" ]; then
   run=true
 fi
 
+# sing == true => use singularity
+# sing == false => use docker
+sing=false
+if [ "$5" == "sing" ]; then
+  sing=true
+fi
+
 
 
 
@@ -33,14 +40,33 @@ fi
 
 #docker run --rm -it -euid=1000 -egid=1000 -etheme=light -ewho=flow -ehome=/mnt//home/paulie -v //home/paulie:/mnt//home/paulie -w //home/paulie/Workspace/Endorse-2Dtest-Bayes -v //home/paulie/Workspace/Endorse-2Dtest-Bayes://home/paulie/Workspace/Endorse-2Dtest-Bayes -v //home/paulie/Workspace://home/paulie/Workspace flow123d/geomop:master_8c1b58980 bash -c "$command"
 
-
+# run sampling
 if [ "$run" == true ]; then
-  command="source ./venv/bin/activate ; python3 -m mpi4py run_all.py $problem_path $output_dir $n_chains"
-  ./endorse_fterm exec "bash -c \"$command\""
+  command="source ./venv/bin/activate && python3 -m mpi4py run_all.py $problem_path $output_dir $n_chains"
 fi
 
 # visualize
 if [ "$visualize" == true ]; then
-  command="source ./venv/bin/activate ; python3 run_all.py $problem_path $output_dir $n_chains visualize"
-  ./endorse_fterm exec "bash -c \"$command\""
+  command="source ./venv/bin/activate && python3 run_all.py $problem_path $output_dir $n_chains visualize"
+fi
+
+
+if [ "$sing" == true ]; then
+
+  # command for running correct docker image
+  rep_dir=$(pwd)
+  image=$(./endorse_fterm image)
+  sing_command="singularity exec -B $rep_dir:$rep_dir docker://$image"
+
+  # auxiliary command for opening Python environment inside docker image
+#  bash_py="bash -c 'source ./venv/bin/activate &&"
+
+  # run setup, prepare PBS script (locally, single proc)
+#  command="$sing_command $bash_py python3 -m mpi4py run_all.py $problem_path $output_dir $n_chains'"
+  command="$sing_command bash -c \"$command\""
+  echo $command
+  eval $command
+
+else
+    ./endorse_fterm exec "bash -c \"$command\""
 fi
